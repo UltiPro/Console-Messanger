d0bc4db7e7a70ea71f9b65b3f61f35f51327106c
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-08 21:04:13 +0200

    Update

--- Console Messenger/thread/stoppableThread.py
+++ Console Messenger/thread/stoppableThread.py
@@ -2,9 +2,6 @@ import threading
 
 
 class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
-
     def __init__(self,  *args, **kwargs):
         super(StoppableThread, self).__init__(*args, **kwargs)
         self._stop_event = threading.Event()
