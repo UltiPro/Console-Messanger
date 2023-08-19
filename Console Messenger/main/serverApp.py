8c384c236fb84080b6635a832b30f272f258b378
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-08-20 00:34:14 +0200

    Update serverApp.py

--- Console Messenger/main/serverApp.py
+++ Console Messenger/main/serverApp.py
@@ -37,13 +37,13 @@ class ServerConsoleMessanger(ConsoleMessanger):
         while self.__running:
             try:
                 cmd = input(
-                    "(Type /help for more informations)\nType command: ").split(" ")
+                    "Type command: (Type /help for more informations)\n").split(" ")
                 match cmd[0]:
                     case "/stop":
                         self._stop()
                     case "/clear":
                         self._clear_console()
-                        self._print_system_information("Console cleared... ")
+                        self._print_system_command("Console cleared... ")
                     case "/msg":
                         message = " ".join(cmd[1:]).strip()
                         if (message == ""):
@@ -52,13 +52,11 @@ class ServerConsoleMessanger(ConsoleMessanger):
                     case "/kick":
                         if (cmd[1].replace(" ", "") == ""):
                             raise IndexError
-                        self._command_kick(cmd[1])
-                    # tutaj
+                        self._command_kick(cmd[1], None)
                     case "/admin":
                         if (cmd[1].replace(" ", "") == ""):
                             raise IndexError
                         self._command_admin(cmd[1])
-                        print(self.__clients_admins_list) # test only
                     case "/unadmin":
                         if (cmd[1].replace(" ", "") == ""):
                             raise IndexError
@@ -66,15 +64,15 @@ class ServerConsoleMessanger(ConsoleMessanger):
                     case "/ban":
                         if (cmd[1].replace(" ", "") == ""):
                             raise IndexError
-                        self._command_ban(cmd[1])
+                        self._command_ban(cmd[1], None)
                     case "/unban":
                         if (cmd[1].replace(" ", "") == ""):
                             raise IndexError
-                        self._command_unban(cmd[1])
+                        self._command_unban(cmd[1], None)
                     case "/list":
                         if (cmd[1].replace(" ", "") == ""):
                             raise IndexError
-                        self._command_list(cmd[1])
+                        self._command_list(cmd[1], None)
                     case "/help":
                         self._command_help()
                     case _:
@@ -85,7 +83,6 @@ class ServerConsoleMessanger(ConsoleMessanger):
             except KeyboardInterrupt:
                 self._stop()
                 break
-            # tutaj
         exit(0)
 
     def _receive_connection(self):
@@ -95,10 +92,10 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 client, address = self.__server.accept()
                 init_data = client.recv(1024).decode("utf-8").split("$$$$")
                 client.send("{}$$$${}".format(e, n).encode("utf-8"))
-                if (init_data[0] in self.__banned_nicknames_list):
-                    raise BannedUserNickname
                 if (address[0] in self.__banned_ips_list):
                     raise BannedUserIp
+                if (init_data[0] in self.__banned_nicknames_list):
+                    raise BannedUserNickname
                 if (init_data[0] in self.__clients_nicknames_list):
                     raise NicknameAlreadyTaken
                 self.__clients_list.append(client)
@@ -108,45 +105,43 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 self.__clients_threads_list.append(StoppableThread(
                     target=self._handle_client, args=(client, )))
                 self.__clients_threads_list[-1].start()
-                self._print_system_information(
-                    "User '{}' connected from '{}'.".format(init_data[0], address))
                 self._broadcast(
                     "Info: {} has join the chat.".format(init_data[0]), client)
-                # tutaj
-            except BannedUserNickname:
-                self._print_system_error(
-                    "Connection from '{}' rejected. Nickname '{}' is banned.".format(address[0], init_data[0]))
-                client.send("Error: Your nickname is banned at this server.")
-                client.close()
+                self._print_system_information(
+                    "User '{}' connected from '{}'.".format(init_data[0], address))
             except BannedUserIp:
+                client.send(RSAImplementation.encrypt_msg_default(
+                    "Error: You are banned at this server.", int(init_data[1]), int(init_data[2])).encode("utf-8"))
                 self._print_system_error(
                     "Connection from '{}' rejected. Address IP banned.".format(address[0]))
-                client.send("Error: You are banned at this server.")
+                client.close()
+            except BannedUserNickname:
+                client.send(RSAImplementation.encrypt_msg_default(
+                    "Error: Your nickname is banned at this server.", int(init_data[1]), int(init_data[2])).encode("utf-8"))
+                self._print_system_error(
+                    "Connection from '{}' rejected. Nickname '{}' is banned.".format(address[0], init_data[0]))
                 client.close()
             except NicknameAlreadyTaken:
-                client.send(
-                    "Info: This nickname is already taken. Choose another one.")
+                client.send(RSAImplementation.encrypt_msg_default(
+                    "Info: This nickname is already taken. Choose another one.", int(init_data[1]), int(init_data[2])).encode("utf-8"))
                 client.close()
+            except (ValueError, UnboundLocalError):
                 # tutaj
-            except OSError:
-                break
-            '''except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
-                if client in self.__clients_list:
-                    self._close_connection(client)
-                else:
-                    client.close()
-            except (OSError, UnboundLocalError, ValueError):
                 if client and address:
                     self._print_system_error(
                         "Incorrect data from '{}', connection denied. More client info: {}.".format(address, client))
                     client.send(
                         "Error: Server connection error. Connection denied.")
-                    if client in self.__clients_list:
-                        self._close_connection(client)
-                    else:
-                        client.close()
+                    self._close_connection(client)
+                # tutaj
                 else:
-                    break'''
+                    continue
+            except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
+                self._print_system_error(
+                    "Connection from '{}' canceled. Connection error.".format(address[0]))
+                self._close_connection(client)
+            except OSError:
+                self._stop()
 
     def _handle_client(self, client):
         nickname = self.__clients_nicknames_list[self.__clients_list.index(
@@ -158,7 +153,6 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 if (message.startswith("/")):
                     message_command = message.split(" ")
                     match message_command[0]:
-                        # tutaj
                         case "/kick":
                             if (client not in self.__clients_admins_list):
                                 raise UnauthorizedError
@@ -170,23 +164,25 @@ class ServerConsoleMessanger(ConsoleMessanger):
                                 raise UnauthorizedError
                             if (message_command[1].replace(" ", "") == ""):
                                 raise IndexError
-                            # self._command_ban(cmd[1])
+                            self._command_ban(message_command[1], client)
                         case "/unban":
                             if (client not in self.__clients_admins_list):
                                 raise UnauthorizedError
                             if (message_command[1].replace(" ", "") == ""):
                                 raise IndexError
-                            # self._command_unban(cmd[1])
+                            self._command_unban(message_command[1], client)
                         case "/list":
                             if (message_command[1].replace(" ", "") == ""):
                                 raise IndexError
-                            # self._command_list(cmd[1])
+                            self._command_list(message_command[1], client)
+                        # tutaj
                         case "/pv":
                             if (message_command[1].replace(" ", "") == "" or message_command[2].replace(" ", "") == ""):
                                 self._send_to(
                                     client, "Error: This command requires two parameters (/pv [nickname] [message]). Try again.")
                             else:
                                 pass  # to
+                        # tutaj
                         case _:
                             self._send_to(
                                 client, "Error: Unknown command. Try again.")
@@ -199,9 +195,9 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 self._send_to(
                     client, "Error: This command requires parameter. Try again.")
             except (ConnectionError, ConnectionResetError, ConnectionAbortedError, OSError):
+                self._print_system_error(
+                    "Error: Connection with '{}' has been lost.".format(nickname))
                 self._close_connection(client)
-                break
-            # tutaj
 
     def _broadcast(self, message, skip_client):
         if message == "":
@@ -214,7 +210,7 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 client.send(RSAImplementation.encrypt_msg_default(
                     message, e, n).encode("utf-8"))
             except (ConnectionError, ConnectionResetError, ConnectionAbortedError):
-                self._close_connection(client)  # to
+                self._close_connection(client)
             '''except ValueError:
                 continue
             '''
@@ -227,24 +223,24 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 to_client)]
             to_client.send(RSAImplementation.encrypt_msg_default(
                 message, e, n).encode("utf-8"))
+        except ValueError:
+            return
         except (ConnectionAbortedError, ConnectionResetError, ConnectionError):
-            pass  # to
-        '''except ValueError:
-                continue
-            '''
+            self._close_connection(to_client)
 
     def _stop(self):
-        self._print_system_information("Stopping the server...")
+        self._print_system_command("Stopping the server...")
         self.__running = False
         self.__receive_connection_thread.stop()
         while (self.__clients_list.__len__() > 0):
             for client in self.__clients_list:
                 self._close_connection(client)
         self.__server.close()
-        self._print_system_information("Server stopped.")
+        self._print_system_command("Server stopped.")
 
     def _close_connection(self, client):
         if client not in self.__clients_list:
+            client.close()
             return
         index = self.__clients_list.index(client)
         nickname = self.__clients_nicknames_list[index]
@@ -256,15 +252,15 @@ class ServerConsoleMessanger(ConsoleMessanger):
         # to
         if client in self.__clients_admins_list:
             self.__clients_admins_list.remove(client)
-            self._print_system_information(
-                "Admin '{}' disconnected.".format(nickname))
             self._broadcast(
                 "Info: Admin {} left the chat!".format(nickname), None)
-        else:
             self._print_system_information(
-                "User '{}' disconnected.".format(nickname))
+                "Admin '{}' disconnected.".format(nickname))
+        else:
             self._broadcast(
                 "Info: User {} left the chat!".format(nickname), None)
+            self._print_system_information(
+                "User '{}' disconnected.".format(nickname))
         # to
         client.close()
         thread.stop()
@@ -294,7 +290,7 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 self.__clients_list[self.__clients_nicknames_list.index(nickname)])
             self._broadcast(
                 "Info: {} has been given admin permissions!".format(nickname), None)
-            self._print_system_information(
+            self._print_system_command(
                 "Info: '{}' has been given admin permissions!".format(nickname))
         except ValueError:
             self._print_system_command(
@@ -306,32 +302,51 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 self.__clients_list[self.__clients_nicknames_list.index(nickname)])
             self._broadcast(
                 "Info: {} has lost admin permissions!".format(nickname), None)
-            self._print_system_information(
+            self._print_system_command(
                 "Info: '{}' has lost admin permissions!".format(nickname))
         except ValueError:
             self._print_system_command(
                 "The given nickname does not match any admin. Try again.")
 
-    # tutaj
-    def _command_ban(self, nickname):
+    def _command_ban(self, nickname, client):
         try:
             client = self.__clients_list[self.__clients_nicknames_list.index(
                 nickname)]
+            self.__banned_nicknames_list.append(nickname)
             self.__banned_ips_list.append(client.getpeername()[0])
+            self._send_to(client, "Ban: You were banned from the chat!")
             self._close_connection(client)
             self._broadcast(
-                "Info: User {} has been banned from the chat!".format(user_nickname), None)
-            self._print_system_information(
-                "Info: User {} has been banned from the chat!".format(user_nickname))
-        except ValueError:
+                "Info: {} has been banned from the chat!".format(nickname), None)
             self._print_system_command(
-                "The given nickname does not match any user. Try again.")
-        except IndexError:
-            self._print_system_error(
-                "This command requires parameter. Try again.")
+                "Info: '{}' has been banned from the chat!".format(nickname))
+        except ValueError:
+            if client:
+                self._send_to(
+                    client, "Info: The given nickname does not match any user. Try again.")
+            else:
+                self._print_system_command(
+                    "The given nickname does not match any user. Try again.")
 
-    def _command_unban(self, user_nickname):
-        pass
+    # tutaj
+    def _command_unban(self, nickname, client):
+        try:
+            index = self.__banned_nicknames_list[self.__banned_nicknames_list.index(
+                nickname)]
+            address = self.__banned_ips_list[index]
+            self.__banned_nicknames_list.remove(nickname)
+            self.__banned_ips_list.remove(address)
+            self._broadcast(
+                "Info: {} has been unbanned from the chat!".format(nickname), None)
+            self._print_system_command(
+                "Info: '{}' has been unbanned from the chat!".format(nickname))
+        except ValueError:
+            if client:
+                self._send_to(
+                    client, "Info: The given nickname does not match any user. Try again.")
+            else:
+                self._print_system_command(
+                    "The given nickname does not match any user. Try again.")
 
     def _command_list(self, command, client):
         message_title = ""
@@ -351,10 +366,6 @@ class ServerConsoleMessanger(ConsoleMessanger):
                 self._print_system_information("\nBanned users list:\n")
                 for banned in self.__banned_nicknames_list:
                     self._print_system_error(banned+" ")
-            case "banned_ip":
-                self._print_system_information("\nBanned ip list:\n")
-                for banned_ip in self.__banned_ips_list:
-                    self._print_system_error(banned_ip+" ")
             case _:
                 self._print_system_command(
                     "The given parameter does not match any type. Try again.")
