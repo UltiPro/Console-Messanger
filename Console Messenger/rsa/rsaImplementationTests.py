c18f7655c211f45367ea86857c5e41180d3ca8cf
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-08-20 03:24:28 +0200

    Update rsaImplementationTests.py

--- Console Messenger/rsa/rsaImplementationTests.py
+++ Console Messenger/rsa/rsaImplementationTests.py
@@ -1,13 +1,13 @@
 from rsaImplementation import RSAImplementation
 
-rsaImplementation = RSAImplementation()
+rsa_implementation = RSAImplementation()
 
 msg = "Example test text"
 
-encrypted_message = rsaImplementation.encrypt_msg(msg)
+encrypted_message = rsa_implementation.encrypt_msg(msg)
 
 print(encrypted_message)
 
-decrypted_message = rsaImplementation.decrypt_msg(encrypted_message)
+decrypted_message = rsa_implementation.decrypt_msg(encrypted_message)
 
 print(decrypted_message)
