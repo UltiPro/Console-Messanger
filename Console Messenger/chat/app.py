36cabe07ea2d0b9ecd64ce81cef0ff3c51ac359d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-08-25 14:27:32 +0200

    Update app.py

--- Console Messenger/chat/app.py
+++ Console Messenger/chat/app.py
@@ -10,25 +10,25 @@ class ConsoleMessanger():
         os.system('cls' if os.name == "nt" else "clear")
 
     def _print_system_information(self, message):
-        print(Fore.GREEN + "{}".format(message) + Style.RESET_ALL)
-
-    def _print_system_information_light(self, message):
         print(Fore.LIGHTGREEN_EX + "{}".format(message) + Style.RESET_ALL)
 
     def _print_system_command(self, message):
-        print(Fore.YELLOW + "{}".format(message) + Style.RESET_ALL)
-
-    def _print_system_command_light(self, message):
         print(Fore.LIGHTYELLOW_EX + "{}".format(message) + Style.RESET_ALL)
 
-    def _print_system_error(self, message):
-        print(Fore.RED + "{}".format(message) + Style.RESET_ALL)
+    def _print_system_command2(self, message):
+        print(Fore.YELLOW + "{}".format(message) + Style.RESET_ALL)
 
-    def _print_system_error_light(self, message):
+    def _print_system_error(self, message):
         print(Fore.LIGHTRED_EX + "{}".format(message) + Style.RESET_ALL)
 
+    def _print_system_ban(self, message):
+        print(Fore.RED + "{}".format(message) + Style.RESET_ALL)
+
+    def _print_system_unban(self, message):
+        print(Fore.GREEN + "{}".format(message) + Style.RESET_ALL)
+
     def _print_system_server_message(self, message):
-        print(Fore.BLUE + "{}".format(message) + Style.RESET_ALL)
+        print(Fore.LIGHTBLUE_EX + "{}".format(message) + Style.RESET_ALL)
 
     def _print_system_private_message(self, message):
-        print(Fore.MAGENTA + "{}".format(message) + Style.RESET_ALL)
+        print(Fore.LIGHTMAGENTA_EX + "{}".format(message) + Style.RESET_ALL)
