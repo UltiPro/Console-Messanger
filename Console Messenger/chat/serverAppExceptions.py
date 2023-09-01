83dc6de977dda911b853656e2125d7d9f8f01bb2
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-09-01 18:14:11 +0200

    Update serverAppExceptions.py

--- Console Messenger/chat/serverAppExceptions.py
+++ Console Messenger/chat/serverAppExceptions.py
@@ -1,18 +1,33 @@
 class BannedUserIp(Exception):
-    pass
+    __message = ">BAN<: You are banned at this server."
+
+    def get_message(self):
+        return self.__message
 
 
 class BannedUserNickname(Exception):
-    pass
+    __message = ">BAN<: Your nickname is banned at this server."
+
+    def get_message(self):
+        return self.__message
 
 
 class NicknameAlreadyTaken(Exception):
-    pass
+    __message = ">ERROR<: This nickname is already taken. Choose another one."
+
+    def get_message(self):
+        return self.__message
 
 
 class NicknameTooShortTooLong(Exception):
-    pass
+    __message = ">ERROR<: Nickname cannot be longer than 24 characters and shorter than 3 characters."
+
+    def get_message(self):
+        return self.__message
 
 
 class UnauthorizedUserAccess(Exception):
-    pass
+    __message = ">ERROR<: This command requires admin permissions."
+
+    def get_message(self):
+        return self.__message
